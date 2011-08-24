881f1e78cdfd8dbb524819c3acc157968d009f40
Your Name <getitnowmarketing@gmail.com>
2011-08-24 09:47:35 -0400

    add freq max at boot

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -22,6 +22,15 @@
 #include <linux/init.h>
 #include "acpuclock.h"
 
+
+/* Make sure the kernel is not overclocked on boot to avoid potential freezing/boot loops
++* for people with less capable hardware. */
+
+#ifdef CONFIG_ACPUCLOCK_OVERCLOCKING
+#define CPUFREQ_MAX 1017600
+#define CPUFREQ_MIN 245760
+#endif
+
 #ifdef CONFIG_MSM_CPU_FREQ_SCREEN
 static void msm_early_suspend(struct early_suspend *handler) {
 	acpuclk_set_rate(CONFIG_MSM_CPU_FREQ_SCREEN_OFF * 1000, 0);
@@ -91,7 +100,22 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	BUG_ON(cpufreq_frequency_table_cpuinfo(policy, table));
 	policy->cur = acpuclk_get_rate();
 	policy->cpuinfo.transition_latency =
-		acpuclk_get_switch_time() * NSEC_PER_USEC;
+
+#ifdef CONFIG_ACPUCLOCK_OVERCLOCKING
+#if 0
+	/* restrict cpu freq scaling range by overwriting */
+	policy->min = CONFIG_MSM_CPU_FREQ_ONDEMAND_MIN;
+	policy->max = CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX;
+#endif
+
+	policy->max = CPUFREQ_MAX;
+	policy->min = CPUFREQ_MIN;
+	policy->cpuinfo.transition_latency =
+	acpuclk_get_switch_time() * NSEC_PER_USEC;
+#else
+	acpuclk_get_switch_time() * NSEC_PER_USEC;
+#endif
+
 	return 0;
 }
 
