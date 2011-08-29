257bac1c97320bdba45c81863e4c76bab897df26
Your Name <getitnowmarketing@gmail.com>
2011-08-29 14:17:23 -0400

    change module dispaly name to greaslightning as "greaselightning" is too many chars and causes run on in scaling_available_governors

--- drivers/cpufreq/cpufreq_greaselightning.c
+++ drivers/cpufreq/cpufreq_greaselightning.c
@@ -163,7 +163,7 @@ static int cpufreq_governor_greaselightning(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_greaselightning = {
-        .name = "greaselightning",
+        .name = "greaslightning",
         .governor = cpufreq_governor_greaselightning,
         .max_transition_latency = 9000000,
         .owner = THIS_MODULE,
